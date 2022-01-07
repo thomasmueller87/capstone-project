90de81b73577dc39d7d5d1c481ea96f5e2bbc575
Thomas Kirst <thomas@moneygarden.de>
2022-01-07 12:04:05 +0100

    Prepared app for deployerchen

--- server/server.js
+++ server/server.js
@@ -1,4 +1,9 @@
 import express from 'express';
+import path from 'path';
+
+import { dirname } from './lib/pathHelpers.js';
+
+const __dirname = dirname(import.meta.url);
 
 const server = express();
 
@@ -6,6 +11,13 @@ const port = process.env.PORT || 4000;
 
 server.get('/api', (req, res) => {
   res.json({ message: 'Hi, express here!' });
+});
+
+// Static assets (images, css, js)
+server.use(express.static(path.join(__dirname, '../client/dist')));
+
+server.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/dist', 'index.html'));
 });
 
 server.listen(port, () =>
