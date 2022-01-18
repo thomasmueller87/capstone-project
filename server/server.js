4b63914cf86157ab90ef1784e8103b25c9708850
Thomas Mueller <thomasmueller87@t-online.de>
2022-01-18 14:33:13 +0100

    Copy of Xmas Backend runs

--- server/server.js
+++ server/server.js
@@ -1,70 +1,44 @@
 import express from 'express';
-import { dirname } from './lib/pathHelpers.js';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
 import path from 'path';
 
-let subscribers = [];
-const __dirname = dirname(import.meta.url);
+import CategoriesRoutes from './routes/categories.routes.js';
+import ProductsRoutes from './routes/products.routes.js';
+
+dotenv.config();
+
+const serverPort = process.env.PORT || 4000;
+
+const dbUser = process.env.DB_USER;
+const dbPassword = process.env.DB_PASSWORD;
+const dbHost = process.env.DB_HOST;
+const dbName = process.env.DB_NAME;
+
+const connectionString = `mongodb+srv://${dbUser}:${dbPassword}@${dbHost}/${dbName}?retryWrites=true&w=majority`;
+
+mongoose.connect(connectionString);
+
+const __dirname = process.cwd();
 
 const server = express();
 
+server.use(cors());
 server.use(express.json());
 
-// new client wants messages
-server.get('/api/subscribe', (req, res) => onSubscribe(req, res));
+server.use('/api', [CategoriesRoutes, ProductsRoutes]);
 
-// sending a message
-server.post('/api/publish', (req, res) => {
-  console.log(req.body);
-  publish(req.body.message); // publish it to everyone
-  res.send('ok');
-});
-
-server.use(express.static(path.join(__dirname, '../client/dist')));
-
-server.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, '../client/dist', 'index.html'));
-});
-
-const port = process.env.PORT || 4000;
-
-const serverInstance = server.listen(port, () =>
-  console.log(`Chat relay server started on port ${port}`)
+server.use(
+  express.static(path.join(__dirname, './client/dist'))
 );
 
-function onSubscribe(req, res) {
-  let id = Math.random();
-
-  res.setHeader('Content-Type', 'text/plain;charset=utf-8');
-  res.setHeader('Cache-Control', 'no-cache, must-revalidate');
-
-  subscribers[id] = res;
-
-  req.on('close', function () {
-    console.log('Close subscribers');
-    delete subscribers[id];
-  });
-}
-
-function publish(message) {
-  for (let id in subscribers) {
-    let res = subscribers[id];
-    res.end(message);
-  }
-
-  subscribers = [];
-}
-
-// delete subscriber when server shuts down
-function closeSubscribers() {
-  for (let id in subscribers) {
-    let res = subscribers[id];
-    res.status(503).end('Server went down for yearly checkup');
-  }
-}
-
-process.on('SIGINT', () => {
-  console.log('SIGINT signal received: closing HTTP server');
-  closeSubscribers();
-  serverInstance.close(() => console.log('Closed express server'));
-  process.exit();
+server.get('/*', function (req, res) {
+  res.sendFile(
+    path.join(__dirname, './client/dist', 'index.html')
+  );
 });
+
+server.listen(serverPort, () =>
+  console.log(`Server is started on port ${serverPort}`)
+);
