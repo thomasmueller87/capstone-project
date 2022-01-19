1e9f114d17bc4b1298859087a887853d67a6385f
Thomas Mueller <thomasmueller87@t-online.de>
2022-01-19 11:04:09 +0100

    fixed conflict inputfield.jsx

--- client/src/components/form/InputField.jsx
+++ client/src/components/form/InputField.jsx
@@ -20,16 +20,8 @@ function InputField({
   return (
     <>
       <Style>
-<<<<<<< HEAD
         <label htmlFor={name}>{children}</label>
         <input
-=======
-        <label className='label' htmlFor={name}>
-          {children}
-        </label>
-        <input
-          className='input'
->>>>>>> main
           type={type}
           id={name}
           name={name}
@@ -50,7 +42,6 @@ function InputField({
 export default InputField;
 
 const Style = styled.div`
-<<<<<<< HEAD
   display: flex;
   justify-content: flex-start;
 
@@ -73,24 +64,6 @@ const Style = styled.div`
   textarea:focus {
     background: rgba(0, 0, 0, 0.1);
     border-color: var(--primary-color);
-=======
-  display: grid;
-  grid-template-columns: 1fr 5fr;
-  grid-template-rows: 2rem;
-
-  .label {
-    grid-area: 1 / 1 / 2 / 2;
-    text-align: left;
-    font-size: 0.7rem;
-    font-weight: normal;
-  }
-  .input {
-    grid-area: 1 / 2 / 2 / 3;
-    background-color: rgba(129, 218, 245, 0.418);
-    border: 1px solid black;
-    padding-left: 5px;
-    width: 100%;
->>>>>>> main
   }
 
   span {
